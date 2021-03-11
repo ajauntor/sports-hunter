70ff8f2201bf0ffc1d480b2b5510145968a68a2f
AJ Auntor <auntorknocks@gmail.com>
2021-03-11 19:15:43 +0600

    added

--- src/component/SportsArea/SprotsArea.js
+++ src/component/SportsArea/SprotsArea.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './SportsArea.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faCoffee,  } from '@fortawesome/free-solid-svg-icons'
 
 const SprotsArea = (props) => {
     // console.log(props);
-    const {strLeague,strTeam, strCountry} = props.sport;
+    const {strLeague,strTeam, strCountry, strTeamLogo, strTeamBadge} = props.sport;
     return (
         <div className="container sports-area">
+            <img src={strTeamBadge} alt=""/>
+            <br/>
+            <br/>
             <h2>{strTeam}</h2>
             <h5>{strLeague}</h5>
             <h6>Country : {strCountry}</h6>
+
+            <button>Explore <FontAwesomeIcon icon={faArrowRight} />
+            </button>
 
         </div>
     );
